d95d6f1dbf506a1388e64b36fb29fd2b627ef8b7
Podcast <arodriguez@valereconsultores.com>
2026-09-09 13:01:31 +0200

    Actualizar PWA para reproductor integrado v13

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-const CACHE = "nh-v12";
-const CORE = ["./", "./index.html", "./styles.css?v=12", "./app.js?v=12", "./manifest.json?v=12", "./icon-192.png", "./icon-512.png"];
+const CACHE = "nh-v13";
+const CORE = ["./", "./index.html", "./styles.css?v=13", "./app.js?v=13", "./manifest.json?v=13", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE).catch(() => {})));
   self.skipWaiting();
 });
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))),
-  );
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener("fetch", (event) => {
